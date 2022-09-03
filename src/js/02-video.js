6348db2216dcacd8bf860c365a8317bdbcea3d51
Ivan Shapovalov <Shapovalov.bizz@gmail.com>
2022-09-03 21:13:55 +0300

    Update

---
+++ src/js/02-video.js
@@ -0,0 +1,22 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const TIME_KEY = 'videoplayer-current-time';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+
+player.on('timeupdate', throttle(setCurrentVideoTime, 1000));
+setVideoTime(localStorage.getItem(TIME_KEY));
+
+
+function setCurrentVideoTime(data) {
+    const currentVideoTime = data.seconds;
+
+    localStorage.setItem(TIME_KEY, currentVideoTime)
+}
+
+function setVideoTime(seconds) {
+    player.setCurrentTime(seconds)
+}
