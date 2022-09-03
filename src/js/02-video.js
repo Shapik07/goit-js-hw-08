46bf6e3a5a6e83cdf69f4515196cb483279a6911
Ivan Shapovalov <Shapovalov.bizz@gmail.com>
2022-09-03 21:34:04 +0300

    Update

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -6,17 +6,15 @@ const TIME_KEY = 'videoplayer-current-time';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-
 player.on('timeupdate', throttle(setCurrentVideoTime, 1000));
 setVideoTime(localStorage.getItem(TIME_KEY));
 
-
 function setCurrentVideoTime(data) {
-    const currentVideoTime = data.seconds;
+  const currentVideoTime = data.seconds;
 
-    localStorage.setItem(TIME_KEY, currentVideoTime)
+  localStorage.setItem(TIME_KEY, currentVideoTime);
 }
 
 function setVideoTime(seconds) {
-    player.setCurrentTime(seconds)
+  player.setCurrentTime(seconds);
 }
