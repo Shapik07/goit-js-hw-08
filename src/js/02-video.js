b76fafc44658bd13f5eb4f43373a2db49475ae86
Ivan Shapovalov <Shapovalov.bizz@gmail.com>
2022-09-03 18:03:10 +0300

    Update

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,7 +1,9 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
 const STORAGE_KEY = 'videoplayer-current-time';
 
 const iframe = document.querySelector('iframe');
 const player = new Vimeo.Player(iframe);
 
+console.log(iframe)
