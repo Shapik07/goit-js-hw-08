14a496725d99b914aeff6ef82c84fd7e16ba26d7
Ivan Shapovalov <Shapovalov.bizz@gmail.com>
2022-09-04 21:16:27 +0300

    Update

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,4 +1,4 @@
-import throttle from 'lodash.throttle'
+import throttle from 'lodash.throttle';
 
 const STORAGE_KEY = 'feedback-form-state';
 
@@ -7,38 +7,37 @@ const formData = {};
 
 const refs = {
   form: document.querySelector('.feedback-form'),
-    textarea: document.querySelector('.feedback-form textarea'),
-  email: document.querySelector('.feedback-form input')
+  textarea: document.querySelector('.feedback-form textarea'),
+  email: document.querySelector('.feedback-form input'),
 };
 
 refs.form.addEventListener('submit', onFormSubmit);
 refs.textarea.addEventListener('input', throttle(onTextareaInput, 500));
 
 refs.form.addEventListener('input', e => {
-    e.preventDefault();
-    formData[e.target.name] = e.target.value;
-    console.log(formData)
-    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
-})
+  e.preventDefault();
+  formData[e.target.name] = e.target.value;
+  console.log(formData);
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+});
 
 populateTextForm();
 populateTextInput();
 
 function onFormSubmit(e) {
-    e.preventDefault();
+  e.preventDefault();
 
-    console.log('Отправляем форму');
-    e.currentTarget.reset();
-    localStorage.removeItem(STORAGE_KEY);
+  console.log('Отправляем форму');
+  e.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
 }
 
-
 function onTextareaInput(e) {
-    const message = e.target.value
-    console.log(message)
+  const message = e.target.value;
+  console.log(message);
 
-    localStorage.setItem(STORAGE_KEY, message);
-};
+  localStorage.setItem(STORAGE_KEY, message);
+}
 
 function populateTextInput() {
   const savedMessage = localStorage.getItem(STORAGE_KEY);
@@ -49,15 +48,10 @@ function populateTextInput() {
 }
 
 function populateTextForm() {
-    const savedMessage = localStorage.getItem(STORAGE_KEY);
+  const savedMessage = localStorage.getItem(STORAGE_KEY);
 
-    if (savedMessage) {
-        console.log(savedMessage);
-        refs.textarea.value = JSON.parse(savedMessage).message;
-    }
-};
-
-
-
-
-
+  if (savedMessage) {
+    console.log(savedMessage);
+    refs.textarea.value = JSON.parse(savedMessage).message;
+  }
+}
